92a0b2ebcceebac4948a3f7e6d47fa4d1cf3ecd1
g-pontes <gui12072004@gmail.com>
2024-05-02 14:42:20 -0300

    hero complete

--- app/page.js
+++ app/page.js
@@ -1,11 +1,13 @@
 import NavBar from '@/src/components/Navbar/NavBar';
 import './page.css'
+import Hero from '@/src/components/Hero/Hero';
 
 export default function Home() {
 
   return (
     <div className="app">
       <NavBar />
+      <Hero />
     </div>
   );
 }
