dbf86950914ee0680283998eceaf681fe19079cd
g-pontes <gui12072004@gmail.com>
2024-05-02 09:54:59 -0300

    first commit

---
+++ app/page.js
@@ -0,0 +1,11 @@
+import NavBar from '@/src/components/Navbar/NavBar';
+import './page.css'
+
+export default function Home() {
+
+  return (
+    <div className="app">
+      <NavBar />
+    </div>
+  );
+}
