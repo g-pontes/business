0b8c1eb9626a6790ac7b97a2c3c647ad4406a2b0
g-pontes <gui12072004@gmail.com>
2024-05-03 09:10:23 -0300

    OurDiff complete

--- app/page.js
+++ app/page.js
@@ -1,17 +1,39 @@
+'use client'
+
 import NavBar from '@/src/components/Navbar/NavBar';
 import './page.css'
 import Hero from '@/src/components/Hero/Hero';
 import BrandingVideo from '@/src/components/BrandingVideo/BrandingVideo';
 import WhatWeDo from '@/src/components/WhatWeDo/WhatWeDo';
+import OurDiff from '@/src/components/OurDiff/OurDiff';
+import { motion, useAnimation } from 'framer-motion';
 
 export default function Home() {
+  const controls = useAnimation()
 
   return (
-    <div className="app">
+    <motion.div className="app" animate={controls}>
       <NavBar />
       <Hero />
       <BrandingVideo />
       <WhatWeDo />
-    </div>
+
+      <motion.div
+        onViewportEnter={() =>
+          controls.start({
+            backgroundColor: 'var(--secondary-color)'
+          })
+        }
+        onViewportLeave={() =>
+          controls.start({
+            backgroundColor: 'white'
+          })
+        }
+        viewport={{amount: 0.4}}
+      >
+        <OurDiff />
+      </motion.div>
+
+    </motion.div>
   );
 }
---
+++ src/components/OurDiff/OurDiff.jsx
@@ -0,0 +1,78 @@
+"use client";
+
+import React from "react";
+import { motion } from "framer-motion";
+import "./ourdiff.css";
+import { ourDiffFeatures } from "@/src/utils/data";
+import Image from "next/image";
+import {
+  containerVariants,
+  descVariants,
+  tagVariants,
+  titleVariants,
+} from "@/src/utils/animation";
+
+const OurDiff = () => {
+  return (
+    <div className="od-wrapper">
+      <div className="container">
+        <div className="od-container">
+          {/* head */}
+          <div className="od-head">
+            <motion.span
+              initial="offscreen"
+              whileInView={"onscreen"}
+              variants={tagVariants}
+              className="tag"
+            >
+              Nossos diferenciais
+            </motion.span>
+            <motion.span
+              initial="offscreen"
+              whileInView={"onscreen"}
+              variants={titleVariants}
+              className="title"
+            >
+              Capital justo, sem complicações
+            </motion.span>
+            <motion.span
+              initial="offscreen"
+              whileInView={"onscreen"}
+              variants={descVariants}
+              className="text"
+            >
+              Nossa missão é nivelar o campo de atuação para o capital de
+              crescimento em estágio inicial.
+              <br /> Fornecemos capital imparcial, flexível e não dilutivo com
+              suporte de execução para acelerar a criação de valor.
+            </motion.span>
+          </div>
+
+          {/* feature */}
+          <div className="od-features">
+            {ourDiffFeatures.map((feature, index) => (
+              <motion.div
+                initial="offscreen"
+                whileInView="onscreen"
+                variants={containerVariants((index + 1) * 0.1)}
+                key={index}
+                className="od-feature"
+              >
+                <Image
+                  src={feature.icon}
+                  alt="feature"
+                  width={128}
+                  height={128}
+                />
+                <span className="title">{feature.title}</span>
+                <span className="text">{feature.des}</span>
+              </motion.div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default OurDiff;
